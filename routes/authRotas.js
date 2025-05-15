06c14e61ab250e8400c91c2e61e13316ae258f81
Guilherme Vidichosqui Men <166948520+Guimenn@users.noreply.github.com>
2025-05-15 14:55:34 -0300

    routes add

---
+++ routes/authRotas.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { loginController } from '../controllers/AuthController.js'
+
+const router = express.Router();
+
+
+router.post('/login', loginController)
+
+
+
+export default router;
