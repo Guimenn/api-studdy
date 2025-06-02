1858d5bf8a781f4a8587b449705c72e78cb00a04
Guilherme Vidichosqui Men <166948520+Guimenn@users.noreply.github.com>
2025-06-02 17:05:49 -0300

    a

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { loginController } from '../controllers/LoginController.js';
+import { loginController, recoveryController, newPasswordController } from '../controllers/LoginController.js';
 import { meController } from '../controllers/MeController.js';
 import authenticated from '../middlewares/authenticated.js';
 
 const router = express.Router();
 
 router.post('/', loginController);
+router.post('/recovery', recoveryController);
+router.put('/new-password', newPasswordController);
 router.get('/', authenticated, meController);
 
 export default router;
